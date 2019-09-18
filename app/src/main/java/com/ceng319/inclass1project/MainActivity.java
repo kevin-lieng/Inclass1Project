fbe68f43596ef4d67ccd8cb249bc9591b6482137
kevin-lieng <48188978+kevin-lieng@users.noreply.github.com>
2019-09-18 13:09:51 -0400

    Added new views with toast message attached to button.

--- app/src/main/java/com/ceng319/inclass1project/MainActivity.java
+++ app/src/main/java/com/ceng319/inclass1project/MainActivity.java
@@ -3,6 +3,8 @@ package com.ceng319.inclass1project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toastMessage(String message){
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
+        toast.show();
+    }
+    public void displayToast(View view) {
+        toastMessage("Hello, this is my first android APP");
     }
 }
